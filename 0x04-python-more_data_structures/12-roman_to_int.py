9377084022043b76a55972250eef33e5877d4498
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2022-09-28 08:08:54 -0800

    updated roman to int py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    if not isinstance(roman_string, str):
+        return 0
     total = 0
-    prev_val = 0
-    for i in range(len(roman_string)):
-        for key, val in num.items():
-            if key == roman_string[i]:
-                total += val
-                if prev_val < val:
-                    total -= (prev_val * 2)
-                    prev_val = val
+    num = 0
+    digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for r in reversed(roman_string):
+        num = digits[r]
+        total += num if total < num * 5 else -num
     return total
