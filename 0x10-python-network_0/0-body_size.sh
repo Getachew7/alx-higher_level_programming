f502040814c04c597a2ebef4b4b52fdb1f072fd4
Getachew7 <69458198+Getachew7@users.noreply.github.com>
2023-01-16 10:05:29 -0800

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # ends a request to that URL displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+curl -si "$1" | grep -i "Content-Length" | cut -d ' ' -f2
